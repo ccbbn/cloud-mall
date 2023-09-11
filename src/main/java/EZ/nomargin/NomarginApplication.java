e24f04542659427a7e0935a83dcd27b09e84aa4e
ccbbn <newve1@naver.com>
2023-09-11 18:42:57 +0900

    dd

--- src/main/java/EZ/nomargin/NomarginApplication.java
+++ src/main/java/EZ/nomargin/NomarginApplication.java
@@ -3,11 +3,18 @@ package EZ.nomargin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
 @SpringBootApplication
 public class NomarginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NomarginApplication.class, args);
+
+
 	}
+
 
 }
