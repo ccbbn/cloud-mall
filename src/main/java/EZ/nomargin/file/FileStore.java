143b6385b615f1482154787df6b706ce057fa286
ccbbn <newve1@naver.com>
2023-09-12 14:59:15 +0900

    dd

--- src/main/java/EZ/nomargin/file/FileStore.java
+++ src/main/java/EZ/nomargin/file/FileStore.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Component
 public class FileStore {
 
-    @Value("${file_dir}")
+    @Value("${file.dir}")
     private String fileDir;
 
     public String getFullPath(String filename) {
